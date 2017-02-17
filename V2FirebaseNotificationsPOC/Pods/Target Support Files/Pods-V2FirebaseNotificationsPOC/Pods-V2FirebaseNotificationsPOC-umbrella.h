ed6f20a2900d2dc0be2544993eb2fb7bc082dda3
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2017-02-17 12:25:08 +0530

    Added Firebase Pod

---
+++ V2FirebaseNotificationsPOC/Pods/Target Support Files/Pods-V2FirebaseNotificationsPOC/Pods-V2FirebaseNotificationsPOC-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_V2FirebaseNotificationsPOCVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_V2FirebaseNotificationsPOCVersionString[];
+
